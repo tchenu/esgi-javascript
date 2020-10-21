0fcf387ca8520d0400caa76a6ce76a3f3484a4be
Thibeault Chenu <contact@tchenu.com>
2020-10-21 08:25:59 +0200

    :sparkles: [DONE] Exercise 1

---
+++ exercise-1/string.js
@@ -0,0 +1,52 @@
+/**
+ * 
+ * @param {*} word 
+ */
+function ucfirst(word)
+{
+    return word.charAt(0).toUpperCase() + word.slice(1)
+}
+
+/**
+ * 
+ * @param {*} word 
+ */
+function lcfirst(word)
+{
+    return word.charAt(0).toLowerCase() + word.slice(1)
+}
+
+function prop_access(object, path) {
+    let result = object
+    path.split('.').forEach(element => {
+        if(result[element]) {
+            return `${path} not exist`
+        }
+
+        if(result[element] === null || result[element] === "") {
+            return object
+        }
+
+        result = result[element]
+    })
+
+    return result
+}
+
+/**
+ * @param {*} characterString 
+ */
+function camelCase(characterString)
+{
+    let words = characterString.split(' ')
+
+    for (index in words) {
+        words[index] = ucfirst(words[index])
+    }
+
+    words[0] = lcfirst(words[0])
+
+    return words.join('')
+}
+
+console.log('camelCase: ' + camelCase('foo bar test'))
