1c0d0ad281bf2d9657e128fd3fc8d61589bbbe9e
Thibeault Chenu <contact@tchenu.com>
2020-10-21 09:03:08 +0200

    Update string.js

--- exercise-1/string.js
+++ exercise-1/string.js
@@ -36,3 +36,13 @@ function prop_access(object, path) {
 
     return result
 }
+
+function camelCase(string) {
+    const separators = [' ', '-', '_'];
+
+    separators.forEach(function (separator) {
+        string = string.replace(separator, '_')
+    })
+
+    return string.split('_').join('')
+}
